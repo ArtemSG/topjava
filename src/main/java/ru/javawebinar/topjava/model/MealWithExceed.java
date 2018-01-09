f9872f5f5170b9ff2d1d47cd0761de22866935f4
ArtemSG <artemsgor@gmail.com>
2018-01-09 22:13:40 +0100

    Web content has benn added(webapp folder, web.xml, index.html, meals.jsp, users.jsp, MealServlet and UserServlet). Added logback.

--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -18,19 +18,13 @@ public class MealWithExceed {
         this.exceed = exceed;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public boolean isExceed() {
-        return exceed;
+    @Override
+    public String toString() {
+        return "UserMealWithExceed{" +
+                "dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                ", exceed=" + exceed +
+                '}';
     }
 }
